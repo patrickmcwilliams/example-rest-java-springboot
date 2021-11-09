83aeffe5266e3a8b4cd56c516f37643cfece7757
Devan Grose <devan.grose@crowdstreet.com>
2021-11-09 14:18:25 -0800

    Move queries to resource file

--- src/main/java/com/crowdstreet/demo/Count/CountController.java
+++ src/main/java/com/crowdstreet/demo/Count/CountController.java
@@ -1,9 +1,5 @@
 package com.crowdstreet.demo.Count;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.http.HttpStatus;
@@ -13,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crowdstreet.demo.Count.model.CreateCountRequest;
@@ -21,6 +16,8 @@ import com.crowdstreet.demo.Count.model.GetCountResponse;
 
 @RestController
 public class CountController {
+	
+	CountResource countResource = new CountResource();
 
 	@GetMapping("/")
 	public String index() {
@@ -32,25 +29,18 @@ public class CountController {
 	        produces = MediaType.APPLICATION_JSON_VALUE)
 	public String testInsert(@RequestBody CreateCountRequest req) throws SQLException {
 		int value = req.getValue();
-		Connection co = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa",""); 
-		String query = "insert into counts(value) values(" + value + ")";
-		PreparedStatement st = co.prepareStatement(query);
-		boolean rs = st.execute();
 		
-		return "Successfully inserted with a value of " + value;
+		boolean success = countResource.insertValue(value);
+		if(success) {
+			return "Successfully inserted with a value of " + value;
+		} else {
+			return "Failed to insert a count with a value of " + value;
+		}
 	}
 	
 	@GetMapping("/count/{id}")
 	public ResponseEntity<GetCountResponse> getCount(@PathVariable long id) throws SQLException {
-		Connection co = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa",""); 
-		String query = "select * from counts where id = "+ (int)id;
-		PreparedStatement st = co.prepareStatement(query);
-		ResultSet rs = st.executeQuery();
-		
-		int value = -1;
-		while(rs.next()) {
-			value = rs.getInt("value");
-		}
+		int value = countResource.getCount(id);
 		
 		GetCountResponse response= new GetCountResponse(value);
 		return new ResponseEntity<>(response, HttpStatus.OK);
